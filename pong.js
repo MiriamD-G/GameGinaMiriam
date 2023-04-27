b62ac38093424e906e8b08d25bef5b3d82927553
GinaBurri <69430435+GinaBurri@users.noreply.github.com>
2023-04-27 13:58:42 +0200

    ins readme übertragen

--- pong.js
+++ pong.js
@@ -1,21 +1,3 @@
-// Vorgehen:
-
-// zwei Rechtecke
-// Rechtecke können sich NUR im Bildschirm bewegen
-// Strich in der Mitte
-// Ball der sich bewegt und in der Y-Achse zurückprallt
-// Ball reagiert auf Rechtecke in der X-Achse
-// Score links und rechts
-// Score wird beim Gegner erhöht, wenn der Ball beim Gegner auf die Werte 0 und Width kommt (so bleibt der Ball im Spiel )
-// Einbauen von gameStates? 
-    // – Start
-    // – Game
-    // Wann ist GameOver?
-
-// Extras:
-// nach jeder ScorErhöhung wird der Ball schneller
-// das Spiel stylen (Farbe, Verschwommenheit, Spuren etc.)
-
 var score = 0
 var b = 15;
 var d = 30;
