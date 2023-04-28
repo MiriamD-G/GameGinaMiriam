3ed5fdea54b6238d88e1b3d3b24805d17f0101af
MiriamD-G <miriam.d@gmx.de>
2023-04-28 22:13:29 +0200

    mit GameStates

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GameGinaMiriam
-Die Farbe der Balken wird intensiver je näher der Ball ist.
+Das Ganze ist eingebettet in das Skript gameStates.js. Somit gibt es drei Stufen welche mit einem Mausklick gesteuert werden. 
 
-Wenn der Ball aus der gegenüberliegenden Seite des Canvas fliegt, zählt ein Punkt und der Ball startet neu in der Mitte. Hier könnten wir eine kleine Startanimation mit Blinken oder so machen.
+Der Ball startet zu schnell, wenn ein Punkt gemacht wurde. Hier wäre vielleicht eine kleine Startanimation oder so noch ganz schön. 
 
-Die Linien auf dem Feld sind nur als Hilfslinien, die müssen nicht bleiben.
+Im dritten Gamestate (Game Over) könnte es eine Animation geben welche den Gewinner verkündet.
+
+Ich überlege mir, dass der Ball jeweils die Farbe annehmen würde von dem Balken, der den letzten Punkt gemacht hat. Erst wenn der Ball mit der eigenen Farbe aus der gegnerischen Seite herausfällt, zählt es einen Punkt hoch.
--- gameStates/gameStates.js
+++ gameStates/gameStates.js
@@ -1,14 +1,32 @@
 let gameState = 0
 let gameStartTime = 0
-let gameDuration = 15
+let gameDuration = 10
 let fontSize = 20
 let hasGameStarted = false
 let timeElapsed
 let ball
 
+var score = 0
+var wBar = 20; // With of bar
+var lBar = 300; // Length of bar
+var dBall = 50; // Diameter of ball
+var speedX = 5;
+var speedY = 10;
+var xBall 
+var yBall 
+let posA = 50 // Position of barA
+let posB = 50 // Position of barB
+let scoreA = 0;
+let scoreB = 0;
+let barSpeed = 15 // Reaktionsgeschwindigkeit der Balken
+
+
 function setup(){
-createCanvas (600, 600)
-ball = new Ball()
+createCanvas (windowWidth, windowHeight)
+// ball = new Ball()
+xBall = width/2
+yBall = height/2
+colorMode(HSB, 360, 100, 100)
 
 }
 
@@ -19,57 +37,115 @@ function draw(){
     } else if (gameState == 1){
         // Spielen
         playGame()
+
     } else if (gameState == 2){
         // Game over
         finishGame()
     }
-    drawTime()
 }
 
 function startGame(){
-    background(0, 200, 80)
+    background(20, 100, 100)
     textAlign(CENTER)
     textSize(fontSize)
     text("START", width/2, height/2)
 
 }
 function playGame(){
-    background(0, 200, 250)
-    textAlign(CENTER)
-    textSize(fontSize)
-    text("SPIELEN", width/2, height/2)
+    background(230, 60, 30)
 
-    ball.show()
-    ball.bounce()
+    // ball.show()
+    // ball.bounce()
     // Spiellogik ...
+
+      // Linien im Spielfeld
+    push()
+    stroke(10, 0, 100)
+    line(width/2, 0, width/2, height)
+    line(0, height/2, width, height/2)
+    pop()
+
+    //Spieler A links
+    let sA = map(xBall, 0, width, 100, 0)
+    fill(150, sA, 100)
+    textSize(50);
+    textAlign(LEFT)
+    text(scoreA, 30, 55);
+    rect(0, posA, wBar, lBar)
+    if (keyIsDown(65) && posA < windowHeight - lBar){ // a nach unten
+        posA += barSpeed
+    }
+    if (keyIsDown(81) && posA > 0){ // q nach oben
+        posA -= barSpeed
+    }
+  
+    //Spieler B rechts
+    let sB = map(xBall, 0, width, 0, 100)
+    fill(350, sB, 100)
+    textSize(50);
+    textAlign(RIGHT);
+    text(scoreB, width - 30, 55);
+    rect(width - wBar, posB, wBar, lBar)
+    if (keyIsDown(76) && posB < windowHeight - lBar) { // l nach unten
+        posB += barSpeed
+    }
+    if (keyIsDown(79) && posB > 0) { // o nach oben
+        posB -= barSpeed
+    }
+    
+    // Ball
+    fill(255)
+    ellipse(xBall, yBall, dBall)
+    xBall += speedX
+    yBall += speedY
+
+    // Abprall von oben und unten
+    if(yBall > height -dBall/2 || yBall < dBall/2) {
+        speedY = -speedY
+    }
+
+    // Abprall vom Spieler A
+    // Liegt: x-Achse = Balken breite und der Hälfte des Balls // y-Achse zwischen den beiden Balken Enden
+    if (xBall < wBar + dBall/2 && yBall > posA && yBall < posA + lBar) {
+        speedX = -speedX
+    // dBall = dBall -10 // reduziert die Ballgrösse bei jedem Abprall
+    }
+
+    // Abprall vom Spieler B
+    // Liegt: x-Achse = WindowWidth - Balken breite und der Hälfte des Balls // y-Achse zwischen den beiden Balken Enden
+    if (xBall > windowWidth - wBar - dBall/2 && yBall > posB && yBall < posB + lBar) {
+        speedX = -speedX
+    // dBall = dBall -10 // reduziert die Ballgrösse bei jedem Abprall
+    }
+
+    if (xBall > windowWidth) {
+        scoreA += 1  
+        xBall = width/2
+        yBall = height/2
+    }
+    if (xBall < 0) {
+        scoreB += 1
+        xBall = width/2
+        yBall = height/2
+    }
+    if (scoreA == 3 || scoreB == 3){
+        gameState = 2
+    }
 }
+
 function finishGame(){
-    background(255, 120, 60)
+    background(150, 60, 100)
     textAlign(CENTER)
     textSize(fontSize)
     text("GAME OVER", width/2, height/2)
 }
 
-function drawTime(){
-    timeElapsed = millis()/1000
-    textSize(10)
-    if(hasGameStarted){
-        let gameTimeElapsed = round(gameDuration - (timeElapsed - gameStartTime))
-        text("verbleibende Zeit: " + gameTimeElapsed, width/2, height - 20)
-    }
-
-    if(timeElapsed - gameStartTime >= gameDuration){
-        gameStartTime = NaN
-        hasGameStarted = false
-        gameState = 2
-    }
-}
 
 function mousePressed(){
     if (gameState == 0){
         gameState = 1
-        gameStartTime = millis()/1000
-        hasGameStarted = true
+        scoreA = 0
+        scoreB = 0
     } else if (gameState == 2){
         gameState = 0
     }
