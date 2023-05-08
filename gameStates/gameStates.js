edf553c4ef42ae3204b3cc773c83ad3173e6af2a
GinaBurri <69430435+GinaBurri@users.noreply.github.com>
2023-05-08 11:42:57 +0200

    test

--- gameStates/gameStates.js
+++ gameStates/gameStates.js
@@ -1,50 +1,46 @@
 let gameState = 0
 let gameStartTime = 0
 let gameDuration = 10
-let fontSize = 40
+let fontSize = 20
 let hasGameStarted = false
 let timeElapsed
 let ball
-let arrow
 
 // test
 // var score = 0
-var wBar = 2; // With of bar
-var lBar = 200; // Length of bar
-var dBall = 10; // Diameter of ball
+var wBar = 20; // With of bar
+var lBar = 300; // Length of bar
+var dBall = 100; // Diameter of ball
 var speedX = 5;
 var speedY = 10;
-var xBall 
-var yBall 
+var xBall
+var yBall
 let posA = 50 // Position of barA
 let posB = 50 // Position of barB
 let scoreA = 0;
 let scoreB = 0;
-let barSpeed = 25 // Reaktionsgeschwindigkeit der Balken
-let delayAfterScore = 3000;
+let barSpeed = 15 // Reaktionsgeschwindigkeit der Balken
+let delayAfterScore = 1000;
 let timeScored = null;
 let scoreUpdated = false;
-let goal = 3 // Anzahl Punkte für den Sieg
 
-function setup(){
-createCanvas (windowWidth, windowHeight)
-// ball = new Ball()
-arrow = new Arrow(100, 70, 20)
-xBall = width/2
-yBall = height/2
-colorMode(HSB, 360, 100, 100)
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    // ball = new Ball()
+    xBall = width / 2
+    yBall = height / 2
+    colorMode(HSB, 360, 100, 100)
 }
 
-function draw(){
-    if(gameState == 0){
+function draw() {
+    if (gameState == 0) {
         // Start
         startGame()
-
-    } else if (gameState == 1){
+    } else if (gameState == 1) {
         // Spielen
         playGame()
 
-    } else if (gameState == 2){
+    } else if (gameState == 2) {
         // Game over
         finishGame()
     }
@@ -53,67 +49,20 @@ function draw(){
 function startGame(){
     background(220, 40, 40)
     textAlign(CENTER)
-    textSize(20)
-    textStyle(BOLD)
-    // text("START", width/2, height/2)
+    textSize(fontSize)
+    text("START", width / 2, height / 2)
 
-    // Q-Pfeil:
-    push()
-    translate(width/2-80, height/2-50)
-    push()
-    stroke(150, 50, 100)
-    // rotate(PI)
-    arrow.show(PI)
-    pop()
-    fill(220, 40, 40)
-    text("Q", 0, 0)
-    pop()
-
-    // A-Pfeil:
-    push()
-    translate(width/2-80, height/2+50)
-    push()
-    stroke(150, 50, 100)
-    arrow.show()
-    pop()
-    fill(220, 40, 40)
-    text("A", 0, 15)
-    pop()
-
-    // O-Pfeil:
-    push()
-    translate(width/2+80, height/2-50)
-    push()
-    stroke(350, 50, 100)
-    rotate(PI)
-    arrow.show()
-    pop()
-    fill(220, 40, 40)
-    text("O", 0, 0)
-    pop()
-
-    // L-Pfeil:
-    push()
-    translate(width/2+80, height/2+50)
-    push()
-    stroke(350, 50, 100)
-    arrow.show()
-    pop()
-    fill(220, 40, 40)
-    text("L", 0, 15)
-    pop()
-    
 }
 
 
 // ! negation von scoreUpdated also wenn der score nicht updated wurde wird er updated
 // score wird gesetzt bevor der Ball wieder neu gestzt wird
 // die aktuelle Zeit (millis()) wird minus die unten gesetzte timeScored gerechnet. Sobald diese grösser wird als die gesetzte delayAfterScore wird der Ball in die Mitte gesetzt
-function resetGame(playerScored){
+function resetGame(playerScored) {
     if (!scoreUpdated) {
         updateScore(playerScored);
     }
-    if (millis() - timeScored > delayAfterScore){
+    if (millis() - timeScored > delayAfterScore) {
         xBall = width / 2;
         yBall = height / 2;
         timeScored = null;
@@ -131,16 +80,16 @@ function updateScore(playerScored) {
     scoreUpdated = true;
 }
 
-function playGame(){
+function playGame() {
     let sB = map(xBall, 0, width, 0, 100)
     let sA = map(xBall, 0, width, 100, 0)
-    background(250, 100, 20)
+    background(sA, 100, sA)
 
     // ball.show()
     // ball.bounce()
     // Spiellogik ...
 
-      // Linien im Spielfeld
+    // Linien im Spielfeld
     push()
     // stroke(10, 0, 100)
     // line(width/2, 0, width/2, height)
@@ -155,13 +104,13 @@ function playGame(){
     textAlign(LEFT)
     text(scoreA, 30, 55);
     rect(0, posA, wBar, lBar)
-    if (keyIsDown(65) && posA < windowHeight - lBar){ // a nach unten
+    if (keyIsDown(65) && posA < windowHeight - lBar) { // a nach unten
         posA += barSpeed
     }
-    if (keyIsDown(81) && posA > 0){ // q nach oben
+    if (keyIsDown(81) && posA > 0) { // q nach oben
         posA -= barSpeed
     }
-  
+
     //Spieler B rechts
     // let sB = map(xBall, 0, width, 0, 100)
     noStroke();
@@ -176,7 +125,7 @@ function playGame(){
     if (keyIsDown(79) && posB > 0) { // o nach oben
         posB -= barSpeed
     }
-    
+
     // Ball
     fill(255)
     ellipse(xBall, yBall, dBall)
@@ -184,72 +133,66 @@ function playGame(){
     yBall += speedY
 
     // Abprall von oben und unten
-    if(yBall > height -dBall/2 || yBall < dBall/2) {
+    if (yBall > height - dBall / 2 || yBall < dBall / 2) {
         speedY = -speedY
     }
 
     // Abprall vom Spieler A
     // Liegt: x-Achse = Balken breite und der Hälfte des Balls // y-Achse zwischen den beiden Balken Enden
-
-    const ballAmLinkenRand = xBall < wBar + dBall/2
-    const ballAufHoeheLinkesPaddle = yBall > posA && yBall < posA + lBar
-
-    const ballAmRechtenRand = xBall > windowWidth - wBar - dBall/2
-    const ballAufHoeheRechtesPaddle = yBall > posB && yBall < posB + lBar
-
-    const abprallLinks = ballAmLinkenRand && ballAufHoeheLinkesPaddle
-    const abprallRechts = ballAmRechtenRand && ballAufHoeheRechtesPaddle
-
-    if (abprallLinks || abprallRechts) {
+    if (xBall < wBar + dBall / 2 && yBall > posA && yBall < posA + lBar) {
         speedX = -speedX
-    } else if (xBall > windowWidth) {
-        // if - Verschachtelung
-        // Anfangswert von timeScored = null
-        // if timeScored == null check bewirkt, dass timeScored nicht jedes mal beim Durchlaufen des draw-loops auf die aktuelle Zeit gesetzt wird
-    
+        // dBall = dBall -10 // reduziert die Ballgrösse bei jedem Abprall
+    }
+
+    // Abprall vom Spieler B
+    // Liegt: x-Achse = WindowWidth - Balken breite und der Hälfte des Balls // y-Achse zwischen den beiden Balken Enden
+    if (xBall > windowWidth - wBar - dBall / 2 && yBall > posB && yBall < posB + lBar) {
+        speedX = -speedX
+        // dBall = dBall -10 // reduziert die Ballgrösse bei jedem Abprall
+    }
+
+    // if - Verschachtelung
+    // Anfangswert von timeScored = null
+    // if timeScored == null check bewirkt, dass timeScored nicht jedes mal beim Durchlaufen des draw-loops auf die aktuelle Zeit gesetzt wird
+    if (xBall > windowWidth) {
         if (timeScored == null) {
             timeScored = millis();
         }
         resetGame('A');
-    } else if (xBall < 0) {
+    }
+
+    if (xBall < 0) {
         if (timeScored == null) {
             timeScored = millis();
         }
         resetGame('B');
     }
-    
-    if (scoreA == goal || scoreB == goal){
+    if (scoreA == 10 || scoreB == 10) {
         gameState = 2
     }
 }
 
-function finishGame(){
+function finishGame() {
     background(150, 60, 100)
     textAlign(CENTER)
     textSize(fontSize)
-    text("The Winner is:", width/2, height/2)
-    if (scoreA == goal){
-        text("A", width/2, height/2 + 40)
-    }
-    if (scoreB == goal){
-        text("B", width/2, height/2 + 40)
-    }
+    text("GAME OVER", width / 2, height / 2)
 }
 
 
-function mousePressed(){
-    if (gameState == 0){
+function mousePressed() {
+    if (gameState == 0) {
         gameState = 1
         scoreA = 0
         scoreB = 0
-    } else if (gameState == 2){
+    } else if (gameState == 2) {
         gameState = 0
     }
 }
 
-// function sleep(millisecondsDuration) {
-//     return new Promise((resolve) => {
-//         setTimeout(resolve, millisecondsDuration);
-//         console.log('bar');
-//     })
-// }
+function sleep(millisecondsDuration) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millisecondsDuration);
+        console.log('bar');
+    })
+}
